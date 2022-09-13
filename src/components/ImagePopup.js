0467b9c555df17afa8f342b2e7e7fe68f4f30503
cactys95 <cactys95@yandex.ru>
2022-09-13 16:38:18 +0300

    feat: небольшие изменения

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,21 +1,27 @@
 const ImagePopup = ({ isOpen, onClose, image }) => {
   return (
-    <div className={`popup popup_type_photo ${isOpen && 'popup_opened'}`}>
-      <div className='popup__container'>
-        <button className='popup__close' type='button' onClick={onClose} />
-        <figure className='photo-container'>
+    <div
+      className={`popup popup_type_photo ${isOpen && 'popup_opened'}`}
+      onClick={onClose}
+    >
+      <div
+        className="popup__container"
+        onClick={(evt) => evt.stopPropagation()}
+      >
+        <button className="popup__close" type="button" onClick={onClose} />
+        <figure className="photo-container">
           <img
-            className='photo-container__photo'
+            className="photo-container__photo"
             alt={image?.title}
             src={image?.imageLink}
           />
-          <figcaption className='photo-container__photo-title'>
+          <figcaption className="photo-container__photo-title">
             {image?.title}
           </figcaption>
         </figure>
       </div>
     </div>
   );
-}
+};
 
 export default ImagePopup;
